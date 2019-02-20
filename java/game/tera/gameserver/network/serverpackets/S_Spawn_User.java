27caf608a8fc2bac1d16d4a7350b2edd0ebb40b9
Pierre <171296@supinfo.com>
2019-02-20 12:27:06 +0100

    Fix user spawn

--- java/game/tera/gameserver/network/serverpackets/S_Spawn_User.java
+++ java/game/tera/gameserver/network/serverpackets/S_Spawn_User.java
@@ -150,7 +150,7 @@ public class S_Spawn_User extends ServerPacket
 		if(newPlayer.hasGuild())
 			packet.writeInt(buffer, newPlayer.getGuild().getAllianceId());//alliance
 		else
-			packet.writeInt(0);
+			packet.writeInt(buffer, 0);
 		packet.writeLong(buffer, 0);
 
 		packet.writeByte(buffer, 1);
